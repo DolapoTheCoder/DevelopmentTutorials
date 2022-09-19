6e50d0c7af8b15d699571f82aea79ec9ff369686
DolapoTheCoder <65259627+DolapoTheCoder@users.noreply.github.com>
2022-09-19 16:34:08 +0100

    voting and adding proposals

--- DAO/smartcontracts/contracts/Dao.sol
+++ DAO/smartcontracts/contracts/Dao.sol
@@ -68,4 +68,61 @@ contract Dao {
         }
         return false;
     }
+
+    //check if someone can vote on a proposal
+    //for requirements in vote and proposals!
+    function checkVoteEligibility(uint256 _id, address _voter)
+        private
+        view
+        returns (bool)
+    {
+        for (uint256 i = 0; i < Proposals[_id].canVote.length; i++) {
+            if (Proposals[_id].canVote[i] == _voter) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    function createProposals(
+        string memory _description,
+        address[] memory _canVote
+    ) public {
+        require(
+            checkProposalEligibility(msg.sender),
+            "Only NFT Holders can put forth Proposals"
+        );
+
+        proposal storage newProposal = Proposals[nextProposal];
+        newProposal.id = nextProposal;
+        newProposal.exists = true;
+        newProposal.descritption = _description;
+        newProposal.deadline = block.number + 100;
+        newProposal.canVote = _canVote;
+        newProposal.maxVotes = _canVote.length;
+
+        emit propsalCreated(
+            nextProposal,
+            _description,
+            _canVote.length,
+            msg.sender
+        );
+        nextProposal++;
+    }
+
+    function voteOnProposal(uint256 _id, bool _vote) public {
+        require(Proposals[_id].exists, "This Proposal doesn't exist.");
+        require(
+            checkVoteEligibility(_id, msg.sender),
+            "You need to be added to canVote"
+        );
+        require(
+            !Proposals[_id].voteStatus[msg.sender],
+            "You've already voted on this proposal."
+        );
+        require(
+            block.number <= Proposals[_id].deadline,
+            "The Deadline has passed for this proposal"
+        );
+    }
 }
