a51733bc57d17302985a23760dc9272e0e922725
DolapoTheCoder <65259627+DolapoTheCoder@users.noreply.github.com>
2022-09-19 14:43:42 +0100

    Dao attributes

--- DAO/smartcontracts/contracts/Dao.sol
+++ DAO/smartcontracts/contracts/Dao.sol
@@ -2,8 +2,70 @@
 
 pragma solidity ^0.8.7;
 
+//interface to check if a user owns an NFT
+interface IdaoContract {
+    function balanceOf(address, uint256) external view returns (uint256);
+}
+
 contract Dao {
     address public owner;
     uint256 nextProposal;
     uint256[] public validTokens;
+    IdaoContract daoContract;
+
+    constructor() {
+        owner = msg.sender;
+        nextProposal = 1;
+        daoContract = IdaoContract(0x2953399124F0cBB46d2CbACD8A89cF0599974963);
+        validTokens = [
+            28683420907420253398025648574484751016562177994318263521192187972719585787905
+        ];
+    }
+
+    struct proposal {
+        uint256 id;
+        bool exists;
+        string descritption;
+        uint256 deadline;
+        uint256 votesUp;
+        uint256 votesDown;
+        address[] canVote;
+        uint256 maxVotes;
+        mapping(address => bool) voteStatus;
+        bool countConducted;
+        bool passed;
+    }
+
+    mapping(uint256 => proposal) public Proposals;
+
+    event propsalCreated(
+        uint256 id,
+        string descriptions,
+        uint256 maxVotes,
+        address proposer
+    );
+
+    event newVote(
+        uint256 votesUp,
+        uint256 votesDown,
+        address voter,
+        uint256 proposal,
+        bool votedFor
+    );
+
+    event propsalCount(uint256 id, bool passed);
+
+    //checks if user owns NFT
+    function checkProposalEligibility(address _proposalList)
+        private
+        view
+        returns (bool)
+    {
+        for (uint256 i = 0; i < validTokens.length; i++) {
+            if (daoContract.balanceOf(_proposalList, validTokens[i]) >= 1) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
