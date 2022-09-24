cfe376908e30e36eb480c37f39b9590c160ae036
DolapoTheCoder <65259627+DolapoTheCoder@users.noreply.github.com>
2022-09-24 20:13:55 +0100

    deposit into staking

--- defi-staking-app/src/contracts/DecentralBank.sol
+++ defi-staking-app/src/contracts/DecentralBank.sol
@@ -8,8 +8,29 @@ contract DecentralBank {
     Tether public tether;
     RWD public rwd;
 
+    addres[] public stakers;
+
+    mapping(address => uint256) public stakingBalance;
+    mapping(address => bool) public hasStaked;
+    mapping(address => bool) public isStaked;
+
     constructor(RWD _rwd, Tether _tether) public {
         rwd = _rwd;
         tether = _tether;
+    }
+
+    //staking function
+    function depostiTokens(uint256 _amount) public {
+        require(_amount > 0, "Amount cannot be 0");
+        tether.transferFrom(msg.sender, address(this), _amount);
+        stakingBalance[msg.sender] += _amount;
+
+        if (!hasStaked[msg.sender]) {
+            stakers.push[msg.sender];
+        }
+
+        //update staking balance
+        isStaked[msg.sender] = true;
+        hasStaked[msg.sender] = true;
     }
 }
