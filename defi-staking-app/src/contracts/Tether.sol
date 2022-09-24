777ed9790e1cce79518474f7e9da40849220b6f7
DolapoTheCoder <65259627+DolapoTheCoder@users.noreply.github.com>
2022-09-24 12:14:34 +0100

    Tether updates

--- defi-staking-app/src/contracts/Tether.sol
+++ defi-staking-app/src/contracts/Tether.sol
@@ -5,4 +5,28 @@ contract Tether {
     string public symbol = "USDT";
     uint256 public totalSupply = 1000000000000000000000000;
     uint8 public decimals = 18;
+
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+
+    event Approve(address indexed _owner, address indexed _spender, uin _value);
+
+    mapping(address => uint256) public balanceOf;
+
+    constructor() public {
+        balanceoF[msg.sender] = totalSupply;
+    }
+
+    function transfer(address _to, uint256 _value)
+        public
+        returns (bool success)
+    {
+        require(
+            balanceOf[msg.sender] >= _value,
+            "Value must be less than balance of user."
+        );
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
 }
