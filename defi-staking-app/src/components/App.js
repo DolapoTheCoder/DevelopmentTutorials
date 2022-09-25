13e47955d6aac6e2d55ab0c15f321749b6db8a66
DolapoTheCoder <65259627+DolapoTheCoder@users.noreply.github.com>
2022-09-25 23:22:07 +0100

    inject meta mask

--- defi-staking-app/src/components/App.js
+++ defi-staking-app/src/components/App.js
@@ -2,8 +2,33 @@
 
 import React, {Component} from 'react';
 import Navbar from './Navbar';
+import Web3 from 'web3';
 
 class App extends Component {
+
+    async UNSAFE_componentWillMount() {
+        await this.loadWeb3()
+        await this.loadBlockchainData()
+    }
+
+    async loadWeb3() {
+        //checks for metamask
+        if(window.ethereum) {
+            window.web3 = new Web3(window.ethereum)
+            await window.ethereum.enable()
+        } else if (window.web3) {
+            //or if we find another provider we go with that...
+            window.web3 = new Web3(window.eth.currentProvider)
+        } else {
+            window.alert('No Ethereum browser detected! Please use MetaMask!')
+        }
+    }
+
+    async loadBlockchainData() {
+        const web3 = window.web3
+        const account = await web3.eth.getAccounts()
+        console.log(account)
+    }
 
     //props sends property from one component to another
     constructor(props) {
@@ -19,9 +44,9 @@ class App extends Component {
         return (
             <div>
                 <Navbar account={this.state.account}/>
-                <div className='text-center'>
-                    <h1>Hello DeCentral Bank</h1>
-                </div>
+                    <div className='text-center'>
+                        <h1></h1>
+                    </div>
             </div>
         )
     }
