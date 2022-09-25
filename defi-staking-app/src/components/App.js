e764fc58338dde3ed03df05ecd275ab868bd3405
DolapoTheCoder <65259627+DolapoTheCoder@users.noreply.github.com>
2022-09-25 22:28:35 +0100

    Add Frontend

---
+++ defi-staking-app/src/components/App.js
@@ -0,0 +1,16 @@
+//Where the main part of the frontend is stored
+
+import React, {Component} from 'react'
+
+class App extends Component {
+    //our react code goes here
+    render() {
+        return (
+            <div>
+                <h1>Hello DeCentral Bank</h1>
+            </div>
+        )
+    }
+}
+
+export default App;
