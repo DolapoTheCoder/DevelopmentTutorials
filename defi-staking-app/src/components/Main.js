e8170b551f09ba42ff01c896fbfc5128ee352185
DolapoTheCoder <65259627+DolapoTheCoder@users.noreply.github.com>
2022-09-26 23:04:58 +0100

    table and form

--- defi-staking-app/src/components/Main.js
+++ defi-staking-app/src/components/Main.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
 import Table from 'react-bootstrap/Table';
+import Form from 'react-bootstrap/Form';
 
 class Main extends Component {
     render() {
         return (
             <div id='content' className='mt-3'>
-                <Table stiped bordered hovers>
+                <Table>
                     <thead>
                         <tr>
                             <th space='col'>Staking Balance</th>
@@ -19,6 +20,14 @@ class Main extends Component {
                         </tr>
                     </tbody>
                 </Table>
+
+                <Form>
+                    <Form.Group className='mb-3' >
+                        <Form.Label><b>Stake tokens Balance:</b></Form.Label>
+                        <Form.Control type='text' placeholder='0' required/>
+                    </Form.Group>
+
+                </Form>
             </div>
         )
     }
