8c95f655e22254d04bba3546cc68c763df6cc344
DolapoTheCoder <65259627+DolapoTheCoder@users.noreply.github.com>
2022-09-27 22:31:58 +0100

    html

--- defi-staking-app/src/components/Main.js
+++ defi-staking-app/src/components/Main.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import Table from 'react-bootstrap/Table';
 import Form from 'react-bootstrap/Form';
+import Card from 'react-bootstrap/Card'
+import tether from'../tether.png'
 
 class Main extends Component {
     render() {
@@ -20,14 +22,27 @@ class Main extends Component {
                         </tr>
                     </tbody>
                 </Table>
-
-                <Form>
-                    <Form.Group className='mb-3' >
-                        <Form.Label><b>Stake tokens Balance:</b></Form.Label>
-                        <Form.Control type='text' placeholder='0' required/>
-                    </Form.Group>
-
-                </Form>
+                <div>
+                    <form>
+                        <div>
+                            <label className='float-left' style={{marginLeft: '15px'}} for='stakeTokenBalance'>
+                                Stake tokens Balance:
+                            </label>
+                            <input type="text" placeholder='0' required id='stakeTokenBalance'/>
+                            <div className='inpute-grouped-open'>
+                                <div className='input-group-text'>
+                                    <img src={tether}alt='tether' height='64'/>  
+                                    &nbsp;&nbsp;&nbsp; USDT
+                                </div>
+                            </div>
+                            <button type='submit' className='btn btn-primary btn-lg btn-black'>Deposit</button>
+                        </div>
+                    </form>
+                    <button className='btn btn-primary btn-lg btn-black'>Withdraw</button>
+                    <div className='card-body text-center' style={{color: 'black'}}>
+                        AIRDROP (add timer)
+                    </div>
+                </div>
             </div>
         )
     }
